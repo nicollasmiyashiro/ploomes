b6452b9abe19c3fa2f9f806ca4cb4fbe50f48930
nicollasmiyashiro <nicollasmiyashirorezende@outlook.com>
2020-06-07 20:53:31 -0300

    Desafio completo

---
+++ API/Api.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Ploomes.API
+{
+    class Api
+    {
+        private static HttpClient client = new HttpClient();
+        private string usrKey = "";
+
+        public Api()
+        {
+            client.BaseAddress = new Uri("https://api2.ploomes.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Key", usrKey);
+        }
+
+        private string DoTask(Task<HttpResponseMessage> task)
+        {
+            task.Wait();
+            task.Result.EnsureSuccessStatusCode();
+
+            var corpoDoRetorno = Task.Run(() => task.Result.Content.ReadAsStringAsync());
+            corpoDoRetorno.Wait();
+
+            return corpoDoRetorno.Result;
+        }
+
+        private string PostSync(string uri, dynamic body)
+        {
+            var retorno = Task.Run(() => client.PostAsync(uri, new StringContent(body.ToString())));
+            return DoTask(retorno);
+        }
+
+        private string PatchSync(string uri, dynamic body)
+        {
+            var retorno = Task.Run(() => 
+                client.SendAsync(new HttpRequestMessage(new HttpMethod("PATCH"), uri)
+                    {
+                        Content = new StringContent(body.ToString())
+                    }
+                )
+            );
+            return DoTask(retorno);
+        }
+
+        public string CreateClient(dynamic body)
+        {
+            return PostSync("/Contacts?$select=Id", body);
+        }
+
+        public string CreateDeal(dynamic body)
+        {
+            return PostSync("/Deals?$select=Id", body);
+        }
+
+        public string CreateTask(dynamic body)
+        {
+            return PostSync("/Tasks?$select=Id", body);
+        }
+
+        public string UpdateDeal(dynamic body, int id = 0)
+        {
+            return PatchSync($"/Deals({id})?$select=Id", body);
+        }
+
+        public string UpdateTask(dynamic body, int id = 0)
+        {
+            return PatchSync($"/Tasks({id})?$select=Id", body);
+        }
+
+        public string FinishTask(int id)
+        {
+            return PostSync($"/Tasks({id})/Finish?$select=Id", new JObject());
+        }
+
+        public string WinDeal(int id)
+        {
+            return PostSync($"Deals({id})/Win?$select=Id", new JObject());
+        }
+
+        public string InteractionRecord(dynamic body)
+        {
+            return PostSync("/InteractionRecords?$select=Id", body);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Ploomes.API;
+using System;
+
+namespace Ploomes
+{
+    class Program
+    {
+        private static Api api;
+        static void Main(string[] args)
+        {
+            api = new Api();
+
+            var idCliente = CriarCliente();
+            var idNegociacao = CriarNegociacao(idCliente);
+            var idTarefa = CriarTarefa(idNegociacao);
+            AtualizarNegociacao(idNegociacao);
+            FecharTarefa(idTarefa);
+            GanharNegociacao(idNegociacao);
+            EscreverNoHistorico(idCliente);
+
+            Console.ReadLine();
+        }
+
+        private static int CriarCliente()
+        {
+            try
+            {
+                dynamic body = new JObject();
+                body.Name = "Cliente";
+                body.Neighborhood = "Vila Guilherme";
+                body.ZipCode = 0;
+                body.CompanyId = null;
+                body.StreetAddressNumber = 123;
+                body.TypeId = 0;
+                body.Phones = JArray.FromObject(new[] { JObject.Parse(@"{PhoneNumber: '(XX) XXXX - XXXX', TypeId: 0, CountryId: 0}") });
+
+                string createClient = api.CreateClient(body);
+                Console.WriteLine("Cliente criado!");
+
+                dynamic obj = JsonConvert.DeserializeObject(createClient);
+                return obj.value[0].Id;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na criação de usuário: \n\n" + e);
+            }
+
+            return 0;
+        }
+
+        private static int CriarNegociacao(int idCliente = 0)
+        {
+            try
+            {
+                dynamic body = new JObject();
+                body.Title = "Negócio";
+                body.ContactId = idCliente;
+                body.Amount = 0;
+                body.StageId = 0;
+
+                string createDeal = api.CreateDeal(body);
+                Console.WriteLine("Negociação criada!");
+
+                dynamic obj = JsonConvert.DeserializeObject(createDeal);
+                return obj.value[0].Id;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro na criação de negociação: \n\n" + e);
+            }
+
+            return 0;
+        }
+
+        private static int CriarTarefa(int idNegociacao = 0)
+        {
+            try
+            {
+                dynamic body = new JObject();
+                body.Title = "Tarefa";
+                body.Description = "Descrição da tarefa";
+                body.DateTime = DateTime.Now.ToString("s");
+                body.EndTime = DateTime.Now.ToString("s");
+                body.ContactId = 0;
+                body.DealId = idNegociacao;
+
+                string createTask = api.CreateTask(body);
+                Console.WriteLine("Tarefa criada!");
+
+                dynamic obj = JsonConvert.DeserializeObject(createTask);
+                return obj.value[0].Id;
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro na criação de tarefa: \n\n" + e);
+            }
+
+            return 0;
+        }
+
+        private static void AtualizarNegociacao(int idNegociacao)
+        {
+            try
+            {
+                dynamic body = new JObject();
+                body.Amount = 15000;
+
+                api.UpdateDeal(body, idNegociacao);
+                Console.WriteLine("Negociação atualizada!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na atualização da negociação: \n\n" + e);
+            }
+        }
+        
+        private static void FecharTarefa(int idTarefa)
+        {
+            try
+            {
+                api.FinishTask(idTarefa);
+                Console.WriteLine("Tarefa fechada!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro no fechamento da tarefa: \n\n" + e);
+            }
+        }
+        
+        private static void GanharNegociacao(int idNegociacao)
+        {
+            try
+            {
+                api.WinDeal(idNegociacao);
+                Console.WriteLine("Negociação Ganha!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao ganhar negociação: \n\n" + e);
+            }
+        }
+
+        private static void EscreverNoHistorico(int idCliente)
+        {
+            try
+            {
+                dynamic body = new JObject();
+                body.ContactId = idCliente;
+                body.Content = "Negócio fechado!";
+                body.Date = DateTime.Now.ToString("s");
+
+                api.InteractionRecord(body);
+
+                Console.WriteLine("Histórico escrito!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro ao escrever no histórico: \n\n" + e);
+            }
+        }
+    }
+}
